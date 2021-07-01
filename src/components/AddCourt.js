6e7f88119f7aea25d94386d96d04440fbfdb6c1c
emills11 <ratdog45@gmail.com>
2021-07-01 16:01:33 -0400

    Added redirect on add court form submission

--- src/components/AddCourt.js
+++ src/components/AddCourt.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Redirect } from 'react-router'
 import '../index.css'
 import AddCourtService from '../services/AddCourtService'
 
@@ -13,6 +14,8 @@ const NewCourt = () => {
     const [country, setCountry] = useState('')
 
     const [radioButton, setRadioButton] = useState('coords')
+
+    const [redirect, setRedirect] = useState(false)
 
     const onSubmit = async (e) => {
         e.preventDefault()
@@ -53,78 +56,82 @@ const NewCourt = () => {
         setZip('')
         setCountry('')
         setRadioButton('coords')
+        setRedirect(true)
     }
 
     return (
-        <form className='form-container' onSubmit={onSubmit}>
-            <div className='form-header'>
-                <p>Is there a court in your area that you'd like to share with the community? Submit a request to have it added to the map!</p>
-                <hr className='solid' />
-            </div>
-            <div className='form-radio' style={{'paddingRight': '6px'}}>
-                <input
-                    type='radio'
-                    id='coords'
-                    value='coords'
-                    name='inputTypeGroup'
-                    checked={radioButton === 'coords'}
-                    onChange={(e) => setRadioButton(e.target.value)} />
-                <label htmlFor='coords'>Court coordinates:</label>
-            </div>
-            <div className='form-sub-container'>
-                <div className='form-row'>
-                    <div className='form-control' style={{'width': '50%'}}>
-                        <label>Longitude</label>
-                        <input type='text' value={longitude} onChange={(e) =>
-                        setLongitude(e.target.value)} disabled={radioButton !== 'coords'} />
-                    </div>
-                    <div className='form-control' style={{'marginLeft': '30px', 'width': '50%'}}>
-                        <label>Latitude</label>
-                        <input type='text' value={latitude} onChange={(e) =>
-                        setLatitude(e.target.value)} disabled={radioButton !== 'coords'} />
-                    </div>
+        <>
+            { redirect ? <Redirect to='/' /> : null}
+            <form className='form-container' onSubmit={onSubmit}>
+                <div className='form-header'>
+                    <p>Is there a court in your area that you'd like to share with the community? Submit a request to have it added to the map!</p>
+                    <hr className='solid' />
                 </div>
-            </div>
-            <div className='form-radio'>
-                <input
-                    type='radio'
-                    id='address'
-                    value='address'
-                    name='inputTypeGroup' 
-                    onChange={(e) => setRadioButton(e.target.value)} />
-                <label htmlFor='address'>Court address info:</label>
-            </div>
-            <div className='form-sub-container'>
-                <div className='form-control'>
-                    <label>Address</label>
-                    <input type='text' value={address} onChange={(e) =>
-                    setAddress(e.target.value)} disabled={radioButton !== 'address'} />
+                <div className='form-radio' style={{'paddingRight': '6px'}}>
+                    <input
+                        type='radio'
+                        id='coords'
+                        value='coords'
+                        name='inputTypeGroup'
+                        checked={radioButton === 'coords'}
+                        onChange={(e) => setRadioButton(e.target.value)} />
+                    <label htmlFor='coords'>Court coordinates:</label>
                 </div>
-                <div className='form-control'>
-                    <label>City</label>
-                    <input type='text' value={city} onChange={(e) =>
-                    setCity(e.target.value)} disabled={radioButton !== 'address'} />
-                </div>
-                <div className='form-row'>
-                    <div className='form-control' style={{'width' : '75%'}}>
-                        <label>State/Province</label>
-                        <input type='text' value={prov} onChange={(e) =>
-                        setProv(e.target.value)} disabled={radioButton !== 'address'} />
-                    </div>
-                    <div className='form-control' style={{'marginLeft': '30px', 'width': '25%'}}>
-                        <label>ZIP/Postal Code</label>
-                        <input type='text' value={zip} onChange={(e) =>
-                        setZip(e.target.value)} disabled={radioButton !== 'address'} />
+                <div className='form-sub-container'>
+                    <div className='form-row'>
+                        <div className='form-control' style={{'width': '50%'}}>
+                            <label>Longitude</label>
+                            <input type='text' value={longitude} onChange={(e) =>
+                            setLongitude(e.target.value)} disabled={radioButton !== 'coords'} />
+                        </div>
+                        <div className='form-control' style={{'marginLeft': '30px', 'width': '50%'}}>
+                            <label>Latitude</label>
+                            <input type='text' value={latitude} onChange={(e) =>
+                            setLatitude(e.target.value)} disabled={radioButton !== 'coords'} />
+                        </div>
                     </div>
                 </div>
-                <div className='form-control'>
-                    <label>Country</label>
-                    <input type='text' value={country} onChange={(e) =>
-                    setCountry(e.target.value)} disabled={radioButton !== 'address'} />
+                <div className='form-radio'>
+                    <input
+                        type='radio'
+                        id='address'
+                        value='address'
+                        name='inputTypeGroup' 
+                        onChange={(e) => setRadioButton(e.target.value)} />
+                    <label htmlFor='address'>Court address info:</label>
                 </div>
-            </div>
-            <button type='submit' className='submit-btn'>Submit</button>
-        </form>
+                <div className='form-sub-container'>
+                    <div className='form-control'>
+                        <label>Address</label>
+                        <input type='text' value={address} onChange={(e) =>
+                        setAddress(e.target.value)} disabled={radioButton !== 'address'} />
+                    </div>
+                    <div className='form-control'>
+                        <label>City</label>
+                        <input type='text' value={city} onChange={(e) =>
+                        setCity(e.target.value)} disabled={radioButton !== 'address'} />
+                    </div>
+                    <div className='form-row'>
+                        <div className='form-control' style={{'width' : '75%'}}>
+                            <label>State/Province</label>
+                            <input type='text' value={prov} onChange={(e) =>
+                            setProv(e.target.value)} disabled={radioButton !== 'address'} />
+                        </div>
+                        <div className='form-control' style={{'marginLeft': '30px', 'width': '25%'}}>
+                            <label>ZIP/Postal Code</label>
+                            <input type='text' value={zip} onChange={(e) =>
+                            setZip(e.target.value)} disabled={radioButton !== 'address'} />
+                        </div>
+                    </div>
+                    <div className='form-control'>
+                        <label>Country</label>
+                        <input type='text' value={country} onChange={(e) =>
+                        setCountry(e.target.value)} disabled={radioButton !== 'address'} />
+                    </div>
+                </div>
+                <button type='submit' className='submit-btn'>Submit</button>
+            </form>
+        </>
     )
 }
 
